2a76cba1fc61dce5b7674c7914b8c43b5ba3a8e1
Him-anshu-Dev10 <himutech10@gmail.com>
2025-07-24 22:16:52 +0530

    variable in js

--- basic_js_tut/basics.js
+++ basic_js_tut/basics.js
@@ -4,3 +4,6 @@ const accoutid = 5;
 accoutName = "Himu";
 // accoutid = 5;
 console.table([accoutName, accoutid]);
+//var can redeclare and updated
+//const not redeclare and not updated
+// let can not redeclare in a scope but can  update

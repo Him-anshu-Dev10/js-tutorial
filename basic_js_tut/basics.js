96316d80007f3bfc905d40128b33c636ac24a4e0
Him-anshu-Dev10 <himutech10@gmail.com>
2025-07-25 15:31:44 +0530

    datatypes in js

--- basic_js_tut/basics.js
+++ basic_js_tut/basics.js
@@ -7,3 +7,13 @@ console.table([accoutName, accoutid]);
 //var can redeclare and updated
 //const not redeclare and not updated
 // let can not redeclare in a scope but can  update
+
+// datatypes
+/*
+Number for number and typeof number is Number itself
+ String for String and Typeof String is String Itselg
+BigInt use for big Number Data typeof BigInt
+Boolean either true or false typeof Boolean
+
+null type of null is object value is not declare 
+undefined value is declared but not defined*/

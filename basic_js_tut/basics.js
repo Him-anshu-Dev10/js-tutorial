c74e3d51fff96278ce7f0087aa270e6edc57c160
Him-anshu-Dev10 <himutech10@gmail.com>
2025-07-25 16:00:56 +0530

    conversions  in js

--- basic_js_tut/basics.js
+++ basic_js_tut/basics.js
@@ -1,10 +1,10 @@
-console.log("himu");
-let accoutName = "Himanshu";
-const accoutid = 5;
-accoutName = "Himu";
-// accoutid = 5;
-console.table([accoutName, accoutid]);
-//var can redeclare and updated
+// console.log("himu");
+// let accoutName = "Himanshu";
+// const accoutid = 5;
+// accoutName = "Himu";
+// // accoutid = 5;
+// console.table([accoutName, accoutid]);
+// //var can redeclare and updated
 //const not redeclare and not updated
 // let can not redeclare in a scope but can  update
 
@@ -17,3 +17,48 @@ Boolean either true or false typeof Boolean
 
 null type of null is object value is not declare 
 undefined value is declared but not defined*/
+
+// conversions in js
+
+// let score = 33;
+// console.log(typeof score);
+// let score = "33";
+// console.log(typeof score);
+// // if we want to convert the srting into number use Number
+// const stringInNum = Number(score);
+// console.log(stringInNum);
+// console.log(typeof stringInNum);
+
+// // if the valriable value is null
+// let num = null;
+// console.table([typeof num, num]);
+// let numb = Number(num);
+// console.table([typeof numb, numb]); // oput = 0 and type number
+
+// // for undefined the output is Nan that means it is not a number
+// // for "33afa" the output is also NaN
+// let age = true;
+// console.log(age);
+// console.log(typeof age);
+// let Age = Number(age);
+// // console.table([typeof Age, Age]); for true the value is = 1 and fales it is 0
+
+// let isLoggedIn = 3;
+// let NewLogin = Boolean(isLoggedIn);
+// console.table([NewLogin]);
+
+// true => true
+// "" =>false
+// "abc"=>true
+// num=> true
+
+let some = undefined;
+let Some = String(some);
+console.log(Some);
+console.log(typeof Some);
+
+// conversion in string;
+// (num) => string;
+
+// undefined => string
+// null=> null

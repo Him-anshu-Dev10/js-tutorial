50b2e77be7fa719f5600537090df26dfe4d2cd08
Him-anshu-Dev10 <himutech10@gmail.com>
2025-07-29 10:39:57 +0530

    Starting of array basics

---
+++ basic_js_tut/array.js
@@ -0,0 +1,27 @@
+// const myArr = [0, 1, 2, 3, 4, 5];
+// push add elemtnt at the last
+// pop remove element from the last
+// unshift  add element in the first index
+// shift remove first element
+// .join changes the array type in to string
+// slice no change in existing array  return the removing val
+// splice change in orignal array remove the return val
+
+// myArr.push(8);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+// myArr.unshift(5);
+// console.log(myArr);
+
+// const myArr = [0, 1, 2, 3, 4, 5];
+// const arr = myArr.slice(1, 3);
+// console.log(arr);
+
+// console.log(myArr);
+// const Arr = myArr.splice(1, 3);
+// console.log(Arr);
+
+// console.log(myArr);

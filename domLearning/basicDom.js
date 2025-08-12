a32103cb6f83bb40f291ae79b1b2f52c98ff7ff3
Him-anshu-Dev10 <himutech10@gmail.com>
2025-08-12 09:31:56 +0530

    01 basic of Dom learning

---
+++ domLearning/basicDom.js
@@ -0,0 +1,25 @@
+// let himu=document.getElementById("head")
+// undefined
+// himu
+// <h1 id=​"head" class=​"himu">​…​</h1>​
+// himu.text
+// undefined
+// himu.textContent
+// 'Welcome to the Himanshu Pokhriyals channel hello '
+// himu.innerHTML
+// 'Welcome to the Himanshu Pokhriyals channel <span id="etc">hello</span> '
+// himu.innerText
+// 'Welcome to the Himanshu Pokhriyals channel hello'
+
+//  the basic  diff bw innerHTMl ,textContent,innerText
+//inner text shows the non hidden data
+//textcontent also shows hidden data
+//innerHtml shows all html inside the box
+
+//  selector => for selecting element in the html for logic implementation
+// id selector document.getElementById("id name")
+//classname selector document.getElementByClassName("class name")
+
+// document.queryselector directly access the element like tag img video heading etc
+// document.queryselector9("tagname" "#id " ".class") for first tag
+// document.querySelectorAll("tagname") for all tags which are written inside queryselectorAll
